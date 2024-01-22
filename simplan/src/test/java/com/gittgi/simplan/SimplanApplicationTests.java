a9aba9aa173a6d2b59db23d258414fc2ab40149f
gittgi <gittgi768@gmail.com>
2024-01-22 14:46:57 +0900

    simplan backend start

---
+++ simplan/src/test/java/com/gittgi/simplan/SimplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gittgi.simplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
